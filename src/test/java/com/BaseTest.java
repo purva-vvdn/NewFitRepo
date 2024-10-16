a294c9bec32683627a360b5a3786f0d197f904cd
root <root@e7061e8585e2>
2024-10-16 15:18:12 +0530

    Pushed Successfully

--- src/test/java/com/BaseTest.java
+++ src/test/java/com/BaseTest.java
@@ -1,23 +1,49 @@
-package com;
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.ExtentTest;
-import com.aventstack.extentreports.reporter.ExtentSparkReporter;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeSuite;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
-public class BaseTest {
-    protected static ExtentReports extent;
-    protected static ExtentTest test;
+public class GoogleSearchTest {
+    WebDriver driver;
 
-    @BeforeSuite
+    @BeforeMethod
     public void setUp() {
-        ExtentSparkReporter sparkReporter = new ExtentSparkReporter("target/ExtentReports.html");
-        extent = new ExtentReports();
-        extent.attachReporter(sparkReporter);
+        // Set the path for the ChromeDriver executable
+        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+        
+        // Initialize ChromeDriver
+        driver = new ChromeDriver();
+        
+        // Maximize the browser window
+        driver.manage().window().maximize();
+        
+        // Open the desired website
+        driver.get("https://www.google.com");
     }
 
-    @AfterSuite
+    @Test
+    public void googleSearchTest() {
+        // Locate the search box
+        WebElement searchBox = driver.findElement(By.name("q"));
+        
+        // Enter a search term
+        searchBox.sendKeys("Selenium WebDriver");
+        
+        // Submit the search form
+        searchBox.submit();
+        
+        // Wait for the results to load and verify the title contains the search term
+        String title = driver.getTitle();
+        Assert.assertTrue(title.contains("Selenium WebDriver"));
+    }
+
+    @AfterMethod
     public void tearDown() {
-        extent.flush();
+        // Close the browser
+        driver.quit();
     }
 }
